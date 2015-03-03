300f391833b087aa7043d71b7a02c4d124f44cd9
Tom Giraudet <tomgiraudet@tom-giraudet.educ.wifi>
2015-03-03 15:06:41 +0100

    Initial commit

---
+++ Gam'Insa/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Gam'Insa
+//
+//  Created by Tom Giraudet on 03/03/2015.
+//  Copyright (c) 2015 Tom Giraudet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var currentPlayer: Player
+    
+    enum CompassPoint {
+        case playing
+        case wino
+        case winx
+        case tie
+    }
+    
+    var game
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
